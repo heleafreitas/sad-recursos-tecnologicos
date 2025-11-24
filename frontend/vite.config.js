740a0cc776ef7a2e270c9b585c0e189ca82c328b
Helen de Freitas <82972511+heleafreitas@users.noreply.github.com>
2025-11-24 17:21:06 -0300

    [fix] chamada da api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'helenapi.perronedev.com.br',
+        target: 'https://helenapi.perronedev.com.br',
         changeOrigin: true
       }
     }
